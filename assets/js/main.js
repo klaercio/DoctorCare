b1b8dccf0be882a5b51aaa547f0bf3107752ca17
klaercio <klaerciosilva@gmail.com>
2022-05-05 11:49:54 -0300

    🚀 Seção serviços finalizada e seção sobre iniciada

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,3 +21,15 @@ function openMenu() {
 function closeMenu() {
     document.body.classList.remove('menu-expanded')
 }
+
+ScrollReveal({
+    origin: 'top',
+    distance: '30px',
+    duration: 1000,
+}).reveal(`
+    #home,
+    #home img,
+    #home .stats,
+    #services,
+    #services header,
+    #services .card`);
